0bc1a7c93cc30c4124fb2d6108d5b6c00d042cfc
devashish bakare <devashishbakare@gmail.com>
2023-08-11 20:25:57 +0530

    added explore section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./Components/Navbar";
 import { HeaderBanner } from "./Components/HeaderBanner";
 import { MobileAnimation } from "./Components/MobileAnimation";
+import { Explore } from "./Components/Explore";
 function App() {
   return (
     <>
@@ -12,6 +13,9 @@ function App() {
       </div>
       <div className="h-[60vh] min-h-[600px] w-full md:h-[70vh]">
         <MobileAnimation />
+      </div>
+      <div className="h-[90vh] min-h-[900px] w-full">
+        <Explore />
       </div>
     </>
   );
---
+++ src/Components/Explore.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Explore = () => {
+  return (
+    <>
+      <div className="h-full w-full  explorBackground bg-cover flex flex-col justify-between">
+        <div className="h-[30%] w-full  flex flex-col gap-10">
+          <span className="max-w-[447px] ml-3 font-gilroy-bold text-[70px] tracking-tighter leading-[60.5px] text-left text-[#f8f8f8] font-bold md:text-[100px] md:max-w-[607px] md:leading-[90.5px]">
+            feel special more often.
+          </span>
+          <span className="font-gilroy-bold ml-3 text-[20px] tracking-tighter leading-[22.5px] text-left text-[#f8f8f8] font-bold">
+            exclusive rewards for paying your bills
+          </span>
+        </div>
+        <div className="flex flex-col gap-5">
+          <span className="max-w-[650px] text-[#f8f8f8] font-bold ml-3">
+            every time you pay your credit cart bills on CRED, you receive CRED
+            coins. you can use these to win exclusive rewards or get special
+            access to curated products and experiences. on CRED, good begets
+            good.
+          </span>
+          <button className="h-[50px] w-[200px] ml-3 bg-[#f8f8f8] font-bold text-[1.2rem] font-gilroy-bold rounded-[50px] md:h-[80px] md:w-[300px] md:text-[1.3rem]">
+            Explore rewards
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
